6997ca07a83603ae1a85f3c0c508f7fdf8e113f2
Joakim Frogner <joakim@ramsvigtech.com>
2016-09-22 20:30:59 +0200

    lol

--- Z-Wave/light.py
+++ Z-Wave/light.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 import re
+import os
 
 user = 'admin'
 password = 'WelcometoCX01'
@@ -15,5 +16,10 @@ if len(sys.argv) > 1:
 		print(re.findall(r"level(.*)", res)[0][3:5])
 	else:
 		session.get("http://10.0.1.12:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_5-0-37/command/{}".format(command), cookies=cookie)
+
+		if command == "on":
+			os.system("say get up you lazy bastard")
+		elif command == "off":
+			os.system("say thank you")
 else:
 	print("This program needs a command: (on / off)")
