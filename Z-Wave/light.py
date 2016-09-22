7f06a48d1ede315e600889246dbe1f72064bd1e7
Joakim Frogner <joakim@ramsvigtech.com>
2016-09-22 20:08:54 +0200

    Modified light.py

--- Z-Wave/light.py
+++ Z-Wave/light.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import re
 
 user = 'admin'
 password = 'WelcometoCX01'
@@ -9,6 +10,10 @@ cookie = {'ZWAYSession':'ceb77ce4-953e-48bf-c747-0104a466bc31'}
 if len(sys.argv) > 1:
 	command = sys.argv[1]
 
-	p = session.get("http://10.0.1.12:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_5-0-37/command/{}".format(command), cookies=cookie)
+	if sys.argv[1] == 'get':
+		res = session.get("http://10.0.1.12:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_5-0-37", cookies=cookie).text
+		print(re.findall(r"level(.*)", res)[0][3:5])
+	else:
+		session.get("http://10.0.1.12:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_5-0-37/command/{}".format(command), cookies=cookie)
 else:
 	print("This program needs a command: (on / off)")
